189e5ad87f7edd57ac5377adfa8fffe3ce563c7f
MarkReddy87 <markreddy87@gmail.com>
2021-07-12 12:56:53 +0000

    add userChoice function and all game choice functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,116 @@
- 
-function runGame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let userChoice = document.getElementsByTagName("button");
 
+    for (let button of userChoice) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "rock") {
+                rock();
+            } else if (this.getAttribute("data-type") === "paper") {
+                paper();
+            } else if (this.getAttribute("data-type") === "scissors") {
+                scissors();
+            } else if (this.getAttribute("data-type") === "lizard") {
+                lizard();
+            } else if (this.getAttribute("data-type") === "spock"){
+                spock();
+            }
+        });
+    }
+ });
+ 
+ function rock () {
+    alert("You chose Rock");
+
+    let choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    let compChoice = choices[Math.floor(Math.random()*5)]
+
+    if (compChoice == 'Rock'){
+        alert("There was a Draw");
+    } else if (compChoice == 'Paper'){
+        alert("You loose :( Computer chose Paper!");
+    } else if (compChoice == 'Scissors'){
+        alert("You Win :D Computer chose Scissors!");
+    } else if (compChoice == 'Lizard') {
+        alert("You Win :D Computer chose Lizard");
+    } else if (compChoice == 'Spock') {
+        alert("You Loose :D Computer chose Spock");
+    }
 }
 
-function checkAnswer() {
+function paper () {
+    alert("You chose Paper!");
 
+    let choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    let compChoice = choices[Math.floor(Math.random()*5)]
+
+    if (compChoice == 'Paper'){
+        alert("There was a Draw");
+    } else if (compChoice == 'Rock'){
+        alert("You Win :D Computer chose Rock!");
+    } else if (compChoice == 'Scissors'){
+        alert("You Loose :( Computer chose Scissors!");
+    } else if (compChoice == 'Lizard') {
+        alert("You Loose :( Computer chose Lizard!");
+    } else if (compChoice == 'Spock') {
+        alert("You Win :D Computer chose Spock!");
+    }
+}
+
+function scissors () {
+    alert("You chose Scissors!");
+
+    let choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    let compChoice = choices[Math.floor(Math.random()*5)]
+
+    if (compChoice == 'Scissors'){
+        alert("There was a Draw");
+    } else if (compChoice == 'Rock'){
+        alert("You Loose :( Computer chose Rock!");
+    } else if (compChoice == 'Paper'){
+        alert("You Win :D Computer chose Paper!");
+    } else if (compChoice == 'Lizard') {
+        alert("You Win :D Computer chose Lizard!");
+    } else if (compChoice == 'Spock') {
+        alert("You Loose :( Computer chose Spock!");
+    }
+}
+
+function lizard () {
+    alert("You chose Lizard!");
+
+    let choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    let compChoice = choices[Math.floor(Math.random()*5)]
+
+    if (compChoice == 'Lizard'){
+        alert("There was a Draw");
+    } else if (compChoice == 'Rock'){
+        alert("You Loose :( Computer chose Rock!");
+    } else if (compChoice == 'Paper'){
+        alert("You Win :D Computer chose Paper!");
+    } else if (compChoice == 'Scissors') {
+        alert("You Loose :( Computer chose Scissors!");
+    } else if (compChoice == 'Spock') {
+        alert("You Win :D Computer chose Spock!");
+    }
+}
+
+function spock () {
+    alert("You chose Spock!");
+
+    let choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+    let compChoice = choices[Math.floor(Math.random()*5)]
+
+    if (compChoice == 'Spock'){
+        alert("There was a Draw");
+    } else if (compChoice == 'Rock'){
+        alert("You Win :D Computer chose Rock!");
+    } else if (compChoice == 'Paper'){
+        alert("You Loose :( Computer chose Paper!");
+    } else if (compChoice == 'Scissors') {
+        alert("You Win :D Computer chose Scissors!");
+    } else if (compChoice == 'Lizard') {
+        alert("You Loose :( Computer chose Spock!");
+    }
 }
 
 function incrementWins() {
